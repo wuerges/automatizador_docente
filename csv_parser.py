fba63242197f57007d5ff3c0add1af3c29704f80
Emilio Wuerges <wuerges@gmail.com>
2017-03-01 16:59:16 -0300

    usado em 2016-2

--- csv_parser.py
+++ csv_parser.py
@@ -7,13 +7,10 @@ l =  []
 with open(sys.argv[1]) as f:
     r = csv.reader(f)
     for row in r:
-        li = []
-        for c in row:
-            li.append(c)
-
+        li = list(row[2:])
         l.append(li)
 
-l = zip(*l)
+l = zip(*l[:2])
 
 print(json.dumps(list(l)))
 
--- removedor.js
+++ removedor.js
@@ -3,12 +3,12 @@
 //
 //
 function removeAll() {
-  el = $(document.getElementById('corpo:j_idt99:0:j_idt117'));
+  el = $("img[title*='Excluir Encontro']").parent();
   if (el) {
     el.click();
     setTimeout(function() { 
       $(document).ready(function () { 
-        $(document.getElementById('corpo:j_idt257')).click();
+        $("span:contains('Confirmar')").parent()[0].click();
         setTimeout(function () { $(document).ready(removeAll); }, 2000);
       }); 
     }, 2000);
