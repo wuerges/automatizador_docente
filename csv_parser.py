169a3aff8a0404c1bcafd2422b4564228d77b669
Emilio Wuerges <wuerges@gmail.com>
2017-12-14 21:32:14 -0200

    Update csv_parser.py

--- csv_parser.py
+++ csv_parser.py
@@ -7,11 +7,8 @@ l =  []
 with open(sys.argv[1]) as f:
     r = csv.reader(f)
     for row in r:
-        li = list(row[2:])
+        li = list(row)
         l.append(li)
 
-l = zip(*l[:2])
-
 print(json.dumps(list(l)))
-
 
